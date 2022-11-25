c93e5869795ca8527e43180a048cb889afd0908b
smiley_12_2 <joseandresgavilanes2012@gmail.com>
2022-11-24 23:30:03 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+"Strings";
+
+//! INTEROS -> INTEGER
+10;
+100000;
+2;
+3;
+4;
+230;
+
+//! FLOAT -> DECIMAL
+1.1;
+3.4;
+3.1416;
+
+//! BOOLEANS
+true;
+false;
+
+//? variables
+
+let marianita = "hola";
+const myConstant = 1;
+
+let pepito = "Hola, cómo estás";
+let age = 18;
+let age2 = 30;
+
+//?Ejercicio
+
+//!Crear una variable que contenga el valor de tu nombre
+//!Crear una variable que contentga el valor de los 4 primeros dígitos de PI
+
+console.log(myConstant);
